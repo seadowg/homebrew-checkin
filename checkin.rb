3724ebd6e34023bc6fd66b3176638169f47ea371
Callum Stott <callum@seadowg.com>
2025-01-22 13:19:15 +0000

    Update checkin.rb

--- checkin.rb
+++ checkin.rb
@@ -2,8 +2,8 @@ class Checkin < Formula
   desc "Quick zipped backups"
   homepage "https://github.com/seadowg/checkin"
   license "GPL-3.0-only"
-  url "https://github.com/seadowg/checkin/releases/download/0.2.0/checkin-0.2.0.zip"
-  sha256 "c2979533f5c28b6dbf4aa96810956ffe12ca24b538111385b7fb80d21f944b75"
+  url "https://github.com/seadowg/checkin/releases/download/0.2.1/checkin-0.2.1.zip"
+  sha256 "0a1fb22c843c0d117a094c032b88dd7d2ac9aba45270ed1d9c9b218def683330"
 
   def install
     system "mkdir", "#{prefix}/bin/"
